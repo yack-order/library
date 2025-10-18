c290786f0ac58256a112d3c66196f4204ae0c30c
flyingtoasters <humor4fun@users.noreply.github.com>
2025-10-17 20:05:05 -0400

    October update
    October update

---
+++ cards/[77sP8]/playlist/audio/Tender Tales - Tender Tales Fifth Collection - Stories for Growing Up [77sP8].m3
@@ -0,0 +1,11 @@
+01 - Hello.m4a
+02 - Birthday Party Bummer.m4a
+03 - Backseat Battle.m4a
+04 - Gift Gloom.m4a
+05 - Reading Roadblock.m4a
+06 - Big Bully.m4a
+07 - Video Game Grumble.m4a
+08 - Splinter Surprise.m4a
+09 - Summer Camp Struggle.m4a
+10 - Katie's Messy Moments.m4a
+11 - Goodbye.m4a
---
+++ cards/[c7k0L]/playlist/audio/Roald Dahl, Sibéal Pounder, Paul King, Simon Farnaby - Wonka (Digital) [c7k0L].m
@@ -0,0 +1,34 @@
+01 - Introduction.m4a
+02 - Prologue.m4a
+03 - Chapter 1 City of Dreams.m4a
+04 - Chapter 2 Scrubbit and Bleacher.m4a
+05 - Chapter 3 Hoverchoc.m4a
+06 - Chapter 4 Always Read the Small Print.m4a
+07 - Chapter 5 Silver Linings.m4a
+08 - Chapter 6 The Vault.m4a
+09 - Chapter 7 The Aristocrat.m4a
+10 - Chapter 8 The Party Choc.m4a
+11 - Chapter 9 The Green Ledger.m4a
+12 - Chapter 10 A Magic Trick.m4a
+13 - Chapter 11 You've Never Had a Chocolate Like This.m4a
+14 - Chapter 12 Gotcha!.m4a
+15 - Chapter 13 The Shop.m4a
+16 - Chapter 14 That Girl.m4a
+17 - Chapter 15 Dog.m4a
+18 - Chapter 16 The Grand Opening.m4a
+19 - Chapter 17 Poison.m4a
+20 - Chapter 18 A One Way Ticket.m4a
+21 - Chapter 19 Settling the Bill.m4a
+22 - Chapter 20 Three Letters.m4a
+23 - Chapter 21 Uncovering the Code.m4a
+24 - Chapter 22 Abigail is on the Loose.m4a
+25 - Chapter 23 Flamingo Jam.m4a
+26 - Chapter 24 The Vault and the Truth.m4a
+27 - Chapter 25 Death by Chocolate.m4a
+28 - Chapter 26 Oompa Loompa in an Elevator.m4a
+29 - Chapter 27 The Empty Jar.m4a
+30 - Chapter 28 Arrest Them.m4a
+31 - Chapter 29 Dreams and Promises.m4a
+32 - Chapter 30 Z.m4a
+33 - Epilogue Wonka.m4a
+34 - Credits.m4a
